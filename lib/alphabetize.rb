c8c407d1b71247a0dec7dd86a30848d5e31792a1
Edwin Lin <edwinlin@gmail.com>
2018-12-17 08:34:26 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-def alphabetize(arr)
-  arr.sort_by do |ele| 
-    ele.each_char.map do |char|
-      ESPERANTO_ALPHABET.index(char)
-    end
-  end
-end
+# def alphabetize(arr)
+#   arr.sort_by do |ele| 
+#     ele.each_char.map do |char|
+#       ESPERANTO_ALPHABET.index(char)
+#     end
+#   end
+# end
