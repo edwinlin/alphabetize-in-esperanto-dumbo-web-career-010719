2d5a3f656e895029c2eab3a36bd6e9cf73eb991c
Edwin Lin <edwinlin@gmail.com>
2018-12-17 08:41:19 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,22 +1,9 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-# def alphabetize(arr)
-#   arr.sort_by do |ele| 
-#     ele.each_char.map do |char|
-#       ESPERANTO_ALPHABET.index(char)
-#     end
-#   end
-# end
-
 def alphabetize(arr)
-p arr
-
-  arr2 = arr.sort_by do |ele| 
-    el = ele.each_char do |char|
+  arr.sort_by do |ele| 
+    ele.each_char.map do |char|
       ESPERANTO_ALPHABET.index(char)
     end
   end
-    p arr2 
-  return arr2.sort_by{|ele| ESPERANTO_ALPHABET.index(ele[0])}
-
 end
